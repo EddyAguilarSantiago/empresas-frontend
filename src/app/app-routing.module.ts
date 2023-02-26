01b5c766961f79976351b721ffa3d6d2e1227588
EddyAguilarSantiago <jesuseduardoaguilars@gmail.com>
2023-02-26 16:33:55 -0600

    Se agrega un navbar y la ruta para crear empresas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListaEmpresasComponent } from './lista-empresas/lista-empresas.component';
+import { RegistrarEmpresaComponent } from './registrar-empresa/registrar-empresa.component';
 
 const routes: Routes = [
   {path: 'empresas', component:ListaEmpresasComponent},
-  {path: '', redirectTo : 'empresas', pathMatch : 'full'}
+  {path: '', redirectTo : 'empresas', pathMatch : 'full'},
+  {path: 'registrar-empresa', component:RegistrarEmpresaComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListaEmpresasComponent } from './lista-empresas/lista-empresas.component';
 import { HttpClientModule } from '@angular/common/http';
+import { RegistrarEmpresaComponent } from './registrar-empresa/registrar-empresa.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListaEmpresasComponent
+    ListaEmpresasComponent,
+    RegistrarEmpresaComponent
   ],
   imports: [
     BrowserModule,
