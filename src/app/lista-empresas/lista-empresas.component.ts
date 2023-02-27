50f64053db43468f8ac122f90c6813e79bb7a094
EddyAguilarSantiago <jesuseduardoaguilars@gmail.com>
2023-02-26 20:22:56 -0600

    Integración de sweet alert

--- src/app/lista-empresas/lista-empresas.component.ts
+++ src/app/lista-empresas/lista-empresas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import  swal  from 'sweetalert2';
 import { Router } from '@angular/router';
 import { Empresa } from '../empresa';
 import { EmpresaService } from '../empresa.service';
@@ -32,9 +33,31 @@ export class ListaEmpresasComponent implements OnInit {
     this.router.navigate(['detalles-empresa', id]);
   }
 
+
   eliminarEmpresa(id:number){
-    this.empresaService.eliminarEmpresa(id).subscribe(dato => {
-      this.obtenerEmpresas();
+    swal({
+      title: '¿Estás seguro?',
+      text: "Confirma si deseas eliminar esta empresa",
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#dc3545',
+      cancelButtonColor: '#6c757d',
+      confirmButtonText: 'Si, eliminar',
+      cancelButtonText: 'No, cancelar',
+      confirmButtonClass: 'btn btn-danger',
+      cancelButtonClass: 'btn btn-secondary',
+      buttonsStyling: true
+    }).then((result) => {
+      if(result.value){
+        this.empresaService.eliminarEmpresa(id).subscribe(dato => {
+          this.obtenerEmpresas();
+          swal(
+            'Empresa eliminada',
+            'La empresa ha sido eliminada con éxito',
+            'success'
+          )
+        })
+      }
     })
   }
 }
