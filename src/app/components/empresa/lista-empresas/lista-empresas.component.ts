f2fcd2c05eddbe472b8ea1970086e080aa3297f4
EddyAguilarSantiago <jesuseduardoaguilars@gmail.com>
2023-03-01 09:53:21 -0600

    Eliminar empresa con usuarios

--- src/app/components/empresa/lista-empresas/lista-empresas.component.ts
+++ src/app/components/empresa/lista-empresas/lista-empresas.component.ts
@@ -3,6 +3,8 @@ import  swal  from 'sweetalert2';
 import { Router } from '@angular/router';
 import { Empresa } from '../../../classes/empresa/empresa';
 import { EmpresaService } from '../../../services/empresa/empresa.service';
+import { Usuario } from 'src/app/classes/usuario/usuario';
+import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-lista-empresas',
@@ -12,15 +14,18 @@ import { EmpresaService } from '../../../services/empresa/empresa.service';
 export class ListaEmpresasComponent implements OnInit {
 
   empresas:Empresa[];
+  usuarios:Usuario[];
   numEmpresas;
   errors=[];
 
-  constructor(private empresaService: EmpresaService, private router:Router) { }
+  constructor(private empresaService: EmpresaService, private usuarioService:UsuarioService, private router:Router) { }
 
   ngOnInit(): void {
+    //Obtenemos la lista de empresas
     this.obtenerEmpresas();
   }
 
+  //Método para obtener empresas
   private obtenerEmpresas(){
     this.empresaService.obtenerListaDeEmpresas().subscribe(empresa => {
       this.empresas = empresa;
@@ -33,10 +38,26 @@ export class ListaEmpresasComponent implements OnInit {
     }, 5000);
   }
 
+  //Método para obtener usuarios
+  private obtenerUsuarios(){
+    this.usuarioService.obtenerListaUsuarios().subscribe(usuario => {
+      this.usuarios = usuario;
+    }, error => this.errors.push(error.message));
+
+    //Vaciamos la variable errors luego de 5 segundos
+    setTimeout(() => {
+      this.errors=[];
+    }, 5000);
+  }
+
   eliminarEmpresa(id:number){
+    //Obtenemos la lista de usuarios
+    this.obtenerUsuarios();
+
+    //Abrimos un mensaje de alerta
     swal({
       title: '¿Estás seguro?',
-      text: "Confirma si deseas eliminar esta empresa",
+      text: "Confirma si deseas eliminar esta empresa, esto también eliminará todos los usuarios que estén en ella",
       type: 'warning',
       showCancelButton: true,
       confirmButtonColor: '#dc3545',
@@ -47,15 +68,29 @@ export class ListaEmpresasComponent implements OnInit {
       cancelButtonClass: 'btn btn-secondary',
       buttonsStyling: true
     }).then((result) => {
+      //Si la opción elegida es eliminar
       if(result.value){
-        this.empresaService.eliminarEmpresa(id).subscribe(empresa => {
-          this.obtenerEmpresas();
-          swal(
-            'Empresa eliminada',
-            'La empresa ha sido eliminada con éxito',
-            'success'
-          )
-        }, error => this.errors.push(error.message));
+        //Hacemos un recorrido de los usuarios
+        for(let usuario of this.usuarios){
+          //Verificamos si el usuario actual es parte de la empresa que se va a eliminar
+          if(usuario.empresa.id == id){
+            //Eliminamos el usuario
+            console.log("Eliminar usuario con id: "+id);
+            this.usuarioService.eliminarUsuario(usuario.id).subscribe(usuario => {}, error => this.errors.push(error.message));
+          }
+        }
+        console.log("Eliminar empresa");
+        //Eliminamos la empresa
+        setTimeout(() => {
+          this.empresaService.eliminarEmpresa(id).subscribe(empresa => {
+            this.obtenerEmpresas();
+            swal(
+              'Empresa eliminada',
+              'La empresa ha sido eliminada con éxito',
+              'success'
+            )}, error => this.errors.push(error.message
+          ));
+        }, 1000);
       }
     });
   }
